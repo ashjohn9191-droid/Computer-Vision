81f312e2fc002fb359932b00b8be4f49ac963690
Ashley <ashjohn9191@gmail.com>
2026-02-17 11:17:57 +0530

    Add files via upload

---
+++ face detection.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+face_classifier = cv2.CascadeClassifier(r"C:\Users\ANITHA\Downloads\haarcascade_frontalface_default.xml")
+
+image = cv2.imread(r"C:\Users\ANITHA\Desktop\Training\Happy\images (3).jpg")
+image = cv2.imread(r"C:\Users\ANITHA\Desktop\Training\Happy\download (4).jpg")
+
+if image is None:
+    print('Error: Image not found or cannot be loaded!')
+    exit()
+    
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = face_classifier.detectMultiScale(gray, 1.3,5)
+
+if len(faces) ==0:
+    print("No faces found")
+else:
+    for(x,y,w,h) in faces:
+        cv2.rectangle(image, (x,y),(x+w, y+h),(127,0,255),2)
+        
+        cv2.imshow('Face Detection', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
